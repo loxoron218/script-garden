ffc0411e2c99621ac8d6241d6edfbfd8bcaaacec
loxoron218 <175253954+loxoron218@users.noreply.github.com>
2025-02-08 13:54:44 +0100

    Repair cleanup

--- arch-postinstallation-nas.sh
+++ arch-postinstallation-nas.sh
@@ -598,7 +598,7 @@ sudo docker compose -f ~/server/immich/docker-compose.yml up -d
 #==============================================================================
 
 ## Remove unnecessary files
-sudo pacman -Rns $(pacman -Qdtq) --noconfirm
+sudo pacman -Rns $(pacman -Qdtq) --noconfirm 2>/dev/null || echo "No orphaned packages to remove"
 sudo pacman -Scc --noconfirm
 sudo rm -rf ~/.cache/go-build
 sudo rm -rf ~/.config/go
